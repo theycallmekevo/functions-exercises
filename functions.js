a47ab0f3be4f453b131c5ebd37bbd86b3e2e0bbd
Kevin Tran <kevin2005tran@gmail.com>
2016-10-27 12:55:59 -0700

    #1 and #2

--- functions.js
+++ functions.js
@@ -1,3 +1,21 @@
-// TODO: add your solutions here!
+//#1 Build your own concatenation
+function concatenation(phrase1, phrase2){
+  console.log(phrase1 + phrase2);
+  return(phrase1 + phrase2);
+};
+// concatenation("Rainbow", "Unicorns");
 
-//test
+
+//#2 Repeat a phrase
+function repeatPhrase(word, n){
+  for (var i = 0; i < n; i++) {
+    console.log(word);
+    return(word);
+  }
+};
+// repeatPhrase("Donuts", 5);
+
+//#3 Build your own Power function
+function powerFunction(a, b){
+
+}
